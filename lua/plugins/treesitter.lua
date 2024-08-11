7c90ce2c60700b4c0cde586cd791077299a8f8e6
Grah <101575465+GRATHRRAM@users.noreply.github.com>
2024-08-11 15:14:59 +0200

    added formaters

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,9 +1,10 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-	    ensure_installed = {"lua" , "c" , "cpp"}
-        highlight = { enable = true }
-        indent = { enable = true }
-    end
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		ensure_installed = { "lua", "c", "cpp" }
+		highlight = { enable = true }
+		indent = { enable = true }
+		auto_install = true
+	end,
 }
