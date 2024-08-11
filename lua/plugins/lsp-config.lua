33c7aa96eaed1ac7347726be37e0bc10c798c4eb
Grah <101575465+GRATHRRAM@users.noreply.github.com>
2024-08-11 13:43:23 +0200

    added lsp server

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,31 @@
+return {
+    { --Mason
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end
+    },
+    { --Mason-lispconfig
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "lua_ls",
+                    "harper_ls"
+                }
+            })
+        end
+    },
+    { --Nvim-lspconfig
+        "neovim/nvim-lspconfig",
+        config = function()
+            local lspconfig = require("lspconfig")
+            lspconfig.lua_ls.setup({})
+            lspconfig.harper_ls.setup({})
+
+            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+            vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, {})
+        end
+    }
+}
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,4 +1,5 @@
 return {
+    {--Telescope
     'nvim-telescope/telescope.nvim',
     tag = '0.1.8',
 	dependencies = { 'nvim-lua/plenary.nvim' },
@@ -7,4 +8,20 @@ return {
         vim.keymap.set('n','<C-p>',builtin.find_files, {})
         vim.keymap.set('n','<C-o>',builtin.live_grep, {})
     end
+    },
+    {--Telescope-ui-select
+        "nvim-telescope/telescope-ui-select.nvim",
+        config = function()
+            require("telescope").setup {
+              extensions = {
+                ["ui-select"] = {
+                  require("telescope.themes").get_dropdown {
+                    -- even more opts
+                  }
+                }
+              }
+            }
+            require("telescope").load_extension("ui-select")
+        end
+    }
 }
