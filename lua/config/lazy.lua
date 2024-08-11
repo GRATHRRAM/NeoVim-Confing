9052e2f5dff49bb1800e159f455210c548fb6e7e
Grah <101575465+GRATHRRAM@users.noreply.github.com>
2024-08-11 12:17:47 +0200

    added neo tree

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -25,9 +25,15 @@ require("lazy").setup({
   spec = {
     -- import your plugins
     { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-		{'nvim-telescope/telescope.nvim', tag = '0.1.8',
-		dependencies = { 'nvim-lua/plenary.nvim' }},
-  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
+	{'nvim-telescope/telescope.nvim', tag = '0.1.8',
+	    dependencies = { 'nvim-lua/plenary.nvim' }},
+    {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+    {"nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",}}
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
