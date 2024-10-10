53a5229e3fde8bebb06cb65637485674b64b93d1
Grah <101575465+GRATHRRAM@users.noreply.github.com>
2024-10-10 11:25:39 +0200

    lspconfig.cmd = {"clangd", "--header-insertion=never"}

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -22,7 +22,9 @@ return {
             local lspconfig = require("lspconfig")
             lspconfig.lua_ls.setup({})
             lspconfig.clangd.setup({})
-
+            
+            lspconfig.cmd = {"clangd", "--header-insertion=never"}
+            
             vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
             vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
             vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, {})
